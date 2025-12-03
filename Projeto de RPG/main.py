52516855737653a56cc4a6851d2930c6bff9bc88
GustavoHenrique-Br <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>
2025-12-03 12:00:12 -0300

    implementação de inimigos

---
+++ Projeto de RPG/main.py
@@ -0,0 +1,120 @@
+import random
+
+#Criação do personagem
+class Personagem:
+    def __init__(self, nome, hp, ataque):
+        self.nome = nome
+        self.hp = hp
+        self.hp_max = hp
+        self.ataque = ataque
+        self.level = 1
+        self.exp = 0
+        self.exp_max = 100
+        self.inventario = ["Poção", "Poção", "Poção"]
+        self.exp_recebido = 0
+
+    #verificação se está vivo
+    def esta_vivo(self):
+        return self.hp > 0
+    #mostrar seus status
+    def mostrar_status(self):
+        print(f"Nome: {self.nome} | Vida: {self.hp}/{self.hp_max}  | Atk máx: {self.ataque} | Level: {self.level} | Exp atual: {self.exp}")
+        
+    def curar(self):
+        if "Poção" in self.inventario:
+            self.inventario.remove("Poção")
+            self.hp += 20
+            print(f"Você usou a poção. Vida atual: {self.hp}/{self.hp_max}")
+            if self.hp > self.hp_max:
+                self.hp = self.hp_max
+        else:
+            print("Não há poções no inventário")
+        
+        
+
+    #função de ataque
+    def atacar(self, alvo):
+        dano_minimo = int(self.ataque / 2)
+        dano_maximo = self.ataque
+        dano = random.randint(dano_minimo, dano_maximo)
+        alvo.hp -= dano
+        if alvo.hp < 0:
+            alvo.hp = 0
+        print(f"{self.nome} atacou {alvo.nome} e tirou {dano} de HP")
+        if dano == dano_maximo:
+            print("Acerto Crítico")
+
+#sistema de ganho de xp
+    def ganhar_xp(self, exp_recebido):   
+        self.exp += exp_recebido
+        print(f"Você ganhou {exp_recebido} de XP! (Total: {self.exp}/{self.exp_max})")
+        if self.exp >= 100:
+            self.exp -= 100
+            self.level += 1
+            self.hp_max += 20
+            self.ataque += 5
+            self.hp = self.hp_max
+            print(f"""
+            ================================
+            UPGRADE! VOCÊ SUBIU PARA O LEVEL {self.level}
+            ================================
+            Novos status:
+            Vida Máxima: {self.hp_max}
+            Ataque Máximo: {self.ataque}
+            XP Atual: {self.exp}/{self.exp_max}
+            ================================
+            """) 
+
+def gerar_inimigos(numero_inimigos):
+    lista_inimigos = ["Orc", "Globin", "Dragão", "Golem", "Mutante"]
+    lista_todos_inimigos = []
+    for x in range (numero_inimigos):
+        nome_sorteado = random.choice(lista_inimigos)
+        hp_sorteado = random.randint(100, 300)
+        ataque_sorteado = random.randint(5, 30)
+        novo_inimigo = Personagem(nome_sorteado, hp_sorteado, ataque_sorteado)
+        lista_todos_inimigos.append(novo_inimigo)
+    return lista_todos_inimigos
+
+        
+
+heroi = Personagem("Gustavo", 125, 25)
+
+
+
+print("----- ÍNICIO DA BATALHA -----")
+heroi.mostrar_status()
+inimigo.mostrar_status()
+print("-" *50)
+
+#loop do combate
+while heroi.esta_vivo() and inimigo.esta_vivo():
+    print(f"\nTurno de {heroi.nome}")
+
+    try:
+        ação = int(input("----> Atacar inimigo (1) | curar (2)? <---- "))
+    except ValueError:
+        print("Isso não é um número")
+    
+    if ação == 1:
+        heroi.atacar(inimigo)
+    elif ação == 2:
+        heroi.curar()
+    else:
+        print("Digito inválido, escolha 1 ou 2.")
+        continue
+    
+
+    if not inimigo.esta_vivo():
+        print(f"\nVocê venceu a luta, você derrotou {inimigo.nome}")
+        xp_ganho = 130
+        heroi.ganhar_xp(xp_ganho)
+        print(f"Status final: {heroi.nome} [Vida:{heroi.hp}] vs {inimigo.nome} [Vida:{inimigo.hp}]")
+        break
+    print("\nTurno do inimigo...")
+    inimigo.atacar(heroi)
+    if not heroi.esta_vivo():
+        print(f"\nVocê perdeu a luta, {inimigo.nome} derrotou você")
+        print(f"Status final: {heroi.nome} [Vida:{heroi.hp}] vs {inimigo.nome} [Vida:{inimigo.hp}]")
+        break   
+    print(f"Status: {heroi.nome} [Vida:{heroi.hp}] vs {inimigo.nome} [Vida:{inimigo.hp}]")
